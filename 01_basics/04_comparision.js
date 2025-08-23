246b3d5c97fa90c691eb1522aca4950750a51359
gouravdenodia2015-create <gouravdenodia2015@gmail.com>
2025-08-23 19:36:57 +0000

    comparisions of values

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 < 1);
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 <= 1);
+// console.log(2 >= 1);
+// console.log(2 != 1);
+
+// console.log("2" < 1);    //here conversion will be done itself thats why u got proper result false
+// console.log("02" < 1);
+
+// console.log(null < 0);      // here sometimes null converted to NaN and sometimes it converted to 0 (int)
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// ===  triple equals to checks comparision strictly including data type
+
+console.log("2" === 2);
+
+
+
+
+
+
+
