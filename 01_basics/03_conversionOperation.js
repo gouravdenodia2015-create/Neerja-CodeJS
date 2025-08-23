3e55a0001979af4b8adcf478f731f44be2c9b261
gouravdenodia2015-create <gouravdenodia2015@gmail.com>
2025-08-23 18:31:00 +0000

    Conversion Data Type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+/*  "33" => 33
+    "33abc" => NaN
+    true => 1   false => 0
+    null => 0
+*/
+
+let isLoggedIn = "N"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+/*
+    into Boolean Conversion
+    1 => true   0=> false
+    "" => false
+    "N" => true
+*/
+
+let anyNumber = 33
+
+let stringAnyNumber = String(anyNumber)
+console.log(typeof stringAnyNumber);
+console.log(stringAnyNumber);
+
+
+
