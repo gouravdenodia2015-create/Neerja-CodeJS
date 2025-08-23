d4c25eb70339d27e99b4a06b9c0896421b9abc70
gouravdenodia2015-create <gouravdenodia2015@gmail.com>
2025-08-23 17:41:28 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 122441
+let accountEmail = "neerja@gmail.com"
+var accountPassword = "123"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2
+
+accountEmail = "abc@abc.com"
+accountPassword = "111"
+accountCity = "Jamnagar"
+
+/*console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";   //teat all JS code as newer version
+
+let age = 18
+let name = "Neerja"
+
+// alert(3 + 3)  // we are using nodeJS not browser
+
+console.log(3 + 3);
+
+// number
+// bigint = used for large numbers
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = type not defined 
+// symbol = unique
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
