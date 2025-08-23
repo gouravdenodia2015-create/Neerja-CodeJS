89c30259296599e837df760c295ee2c5bb8f2a2a
gouravdenodia2015-create <gouravdenodia2015@gmail.com>
2025-08-23 19:04:43 +0000

    Operations on variables

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -5,8 +5,8 @@ let score = true
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
 
 /*  "33" => 33
     "33abc" => NaN
@@ -17,7 +17,7 @@ console.log(valueInNumber);
 let isLoggedIn = "N"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 /*
     into Boolean Conversion
@@ -29,8 +29,49 @@ console.log(booleanIsLoggedIn);
 let anyNumber = 33
 
 let stringAnyNumber = String(anyNumber)
-console.log(typeof stringAnyNumber);
-console.log(stringAnyNumber);
+//console.log(typeof stringAnyNumber);
+//console.log(stringAnyNumber);
+
+// ******************************************** OPERATIONS *****************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3); power
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "hello"
+let str2 = " Neerja"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
 
 
 
