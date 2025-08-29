3af17a28d8a7edacbd0c13611726f3c0e3e425b5
Niraj Varma <gouravdenodia2015@gmail.com>
2025-08-29 12:04:44 +0000

    Objects in JS

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,26 @@
+//  SINGLETON OBJECTS
+//  THE OBJECTS WHICH IS ONLY SINGLE CALLED SINGLETON OBJECTS. THIS TYPE OF OBJECTS CAN BE CREATED USING CONSTRUCTOR
+
+//  OBJECT LITERALS
+
+const mySymbol = Symbol("Something")
+
+const JSUser = {
+    name : "Neerja",
+    "full name" : "Neerja Dinodia",
+    [mySymbol] : "Symbol1",
+    age : 18,
+    email : "neerja@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday", "Saturday"]
+}
+
+console.log(JSUser.email);
+console.log(JSUser["email"]);
+console.log(JSUser["full name"]);
+console.log(JSUser[mySymbol]);
+
+JSUser.email = "neerjadinodia@gmail.com" 
+Object.freeze(JSUser)
+JSUser.age = 21;
+console.log(JSUser)
