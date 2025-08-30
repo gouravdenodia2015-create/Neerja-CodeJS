ada223f685945e9b34a91ab4171e515b2dd9ed1c
Niraj Varma <gouravdenodia2015@gmail.com>
2025-08-30 08:52:18 +0000

    objects in detail

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -32,7 +32,47 @@ const obj2 = {
     4 : "D"
 }
 
+const obj4 = {
+    5 : "E",
+    6 : "F"
+}
 // const obj3 = {obj1, obj2}
 //console.log(obj3);
 
-const obj3 = Object.assign({}, obj1, obj2)
+const obj5 = Object.assign(obj1, obj2)
+const obj3 = Object.assign({}, obj1, obj2, obj4)
+const obj6 = {...obj1, ...obj2, ...obj4}
+
+// console.log(obj3);
+// console.log(obj5);
+// console.log(obj6);
+
+const userObj = [
+    {
+        id: 1,
+        email: "n@gmail.com"
+    },
+    {
+        id: 2,
+        email: "v@gmail.com"
+    },
+    {
+        id: 3,
+        email: "g@gmail.com"
+    }
+]
+
+// console.log(userObj[1].email);
+// console.log(userObj);
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));       //  IMP
+console.log(Object.values(tinderUser));     //  IMP
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("name"));
+console.log(tinderUser.hasOwnProperty("full name"));
+
+
+
+
