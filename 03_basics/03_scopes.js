6b916d4eb5c3655f67ec6616a51c0706aab30406
Niraj Varma <gouravdenodia2015@gmail.com>
2025-09-01 10:46:45 +0000

    scopes in different way

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -6,7 +6,7 @@ if(true)
 {
     let a = 10;
     const b = 20;
-    console.log("Inner A :", a);
+    // console.log("Inner A :", a);
     
 }
 // var c = 3000
@@ -32,4 +32,35 @@ function one()
 
 // one()
 
+if(true)
+{
+    const username = "Neerja"
 
+    if(username === "Neerja")
+    {
+        const website = " Youtube"
+        //console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+//  ++++++++++++++++++++++++++++++++++++++++    INTERESTING     +++++++++++++++++++++++++++++++++++++++++++
+
+console.log(plusOne(5));
+
+function plusOne(num1)
+{
+    return num1 + 1;
+}
+
+
+const addTwo = function(num1)
+{
+    return num1 + 2;
+}
+
+console.log(addTwo(6));
