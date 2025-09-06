e88977eb7b43cb09bad238ef2e97c1a0d28a37f1
Niraj Varma <gouravdenodia2015@gmail.com>
2025-09-06 16:55:50 +0000

    if statement demo

---
+++ 04_controlFlow/01_ifElseDemo.js
@@ -0,0 +1,98 @@
+// IF STATEMENT
+
+const tempreture = 40
+/*
+if(tempreture <= 50)
+{
+    console.log("Less than 50...");
+    
+}
+else
+{
+    console.log("More than 50...");
+    
+}
+*/
+
+const userLogin = true
+
+/*
+if(userLogin)
+{
+    console.log("Execute");
+    
+}
+*/
+
+//  CONDITIONAL OPERATORS USED IN IF STATEMENT
+//  <, <=, >, >=, ==, !=, ===, !==
+
+const score = 200
+
+/*
+if (score > 100)
+{
+    let power = "Fly";
+    console.log(`User power : ${power}`);
+        
+}
+
+console.log(`User power : ${power}`);
+*/
+
+//  SINGLE LINE IF CONDITION
+
+const balance = 1000
+
+//if (balance > 500) console.log("Less than 500");        //  IMPLICIT SCOP, YOU CAN DIRECTLY EXECUTE TRUE STATEMENT LIKE THIS
+
+/*if(balance < 500)
+{
+    console.log("Less than 500");
+    
+}
+else if(balance < 750)
+{
+    console.log("Less than 750");
+        
+}
+else if(balance < 900)
+{
+    console.log("Less than 900");
+    
+}
+else
+{
+    console.log("More than 900");
+    
+}
+*/    
+
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+/*
+if (userLoggedIn && debitCard && loggedInFromGoogle)
+{
+    console.log("Allowed to buy a course...");
+    
+}
+else 
+{
+    console.log("Not Allowed...");
+    
+}
+
+if(!loggedInFromEmail || loggedInFromGoogle || 2 ==3)
+{
+    console.log("Allowed...");
+    
+}
+else 
+{
+    console.log("Not allowed...");
+    
+}*/
