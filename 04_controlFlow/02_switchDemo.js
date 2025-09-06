b137f9cfb71a875ca0265c3b8acd65e6de994f61
Niraj Varma <gouravdenodia2015@gmail.com>
2025-09-06 17:07:36 +0000

    switch Statement Demo

---
+++ 04_controlFlow/02_switchDemo.js
@@ -0,0 +1,71 @@
+//  SWITCH CASE EXAMPLES
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+/*const month = 7;
+
+switch(month)
+{
+    case 1:
+        console.log("January");
+    break;
+
+    case 2:
+        console.log("February");
+    break;
+
+    case 3:
+        console.log("March");
+    break;
+
+    case 4:
+        console.log("April");
+    break;
+
+    case 5:
+        console.log("May");
+    break;
+
+    default:
+        console.log("Invalid");
+    break;
+        
+}*/
+
+const month = "April";
+
+switch(month)
+{
+    case "January":
+        console.log("January");
+    break;
+
+    case "February":
+        console.log("February");
+    break;
+
+    case "March":
+        console.log("March");
+    break;
+
+    case "April":
+        console.log("April");
+    break;
+
+    case "May":
+        console.log("May");
+    break;
+
+    default:
+        console.log("Invalid");
+    break;
+        
+}
