bff3690786a0fae3dc1a66e6b445847b28dd7f62
Niraj Varma <gouravdenodia2015@gmail.com>
2025-09-07 12:34:00 +0000

    while Dowhile loop

---
+++ 05_iterations/02_whileDoWhile.js
@@ -0,0 +1,29 @@
+// WHILE LOOP
+
+let i = 1
+while (i <= 10) {
+
+    //console.log(i);
+    i++;
+    
+}
+
+
+let myArray = ["A", "B", "C"]
+
+let arr = 0
+
+while (arr < myArray.length) {
+
+    //console.log(myArray[arr]);
+    arr = arr + 1;
+}
+
+
+
+let score = 11;
+do {
+    console.log(score);
+    score++;
+
+} while (score <= 10);
